83acd4a5ff8059b2e6555af5a08bbe7d362ebf5d
João Carlos Epifanio <j.carlos.ico@gmail.com>
2019-06-19 17:07:05 -0300

    pagina de configuracao e ajuda

---
+++ app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+   {
+    path: 'configuracao',
+    loadChildren: './configuracao/configuracao.module#ConfiguracaoPageModule'
+  },
+  {
+    path: 'ajuda',
+    loadChildren: './ajuda/ajuda.module#AjudaPageModule'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
